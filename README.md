145a4dc8902e00069d84cc824e947ab783337fff
Carlos Miguel Bustillo Rdguez <20931458+carlosbustillordguez@users.noreply.github.com>
2021-01-04 20:10:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# ECS one-off task
+
+Creates a one-off ECS task.
+
+## Requirements
+
+- The `aws` command line must be installed on the system. See [Installing, updating, and uninstalling the AWS CLI version 2](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html) in the AWS documentation for further details.
+- The command line tool [jq](https://stedolan.github.io/jq/). For Debian/Ubuntu distros: `apt install jq`.
+
+## Usage
+
+Download the script and apply the execution permissions:
+
+```bash
+curl -L https://github.com/speedyrails/ecs-one-off-task/raw/master/ecs-one-off-task.sh -o ecs-one-off-task.sh
+chmod +x ecs-one-off-task.sh
+```
+
+The script spects for the following arguments:
+
+- `TASK_NAME`: ECS definition task name.
+- `EXEC_ROLE_ARN`: ECS execution role for the task. The role must allow read only access to AWS S3 (AWS IAM Managed Policy `AmazonS3ReadOnlyAccess`).
+- `ECS_CLUSTER_NAME`: ECS Cluster name.
+- `OCI_IMAGE:TAG`: the OCI image to use for the container task.
+- `S3_ARN_ENV_VARS`: the AWS S3 ARN with the required environment variables for the container.
+- `COMMAND_ENTRYPOINT`: the entrypoint used by the container.
+- `COMMAND_ARGUMENT`: the command passed to the container's entrypoint.
+
+To execute the task for database migrations:
+
+```bash
+./ecs-one-off-task.sh "TASK_NAME" "EXEC_ROLE_ARN" "ECS_CLUSTER_NAME" "OCI_IMAGE:TAG" "S3_ARN_ENV_VARS" "ENTRYPOINT" "COMMAND"
+```
+
+## Examples
+
+```bash
+./ecs-one-off-task.sh "db-migrations" "arn:aws:iam::<ACCOUNT_ID>:role/ecsTaskExecutionRole" "myEcsCluster" "myapp:latest" "arn:aws:s3:::bucket/myapp-vars.env" "sh -c" "bundle exec rake db:migrate"
+```
+
+## License
+
+MIT
+
+## Author Information
+
+[Speedyrails Inc.](https://www.speedyrails.com/)
+
+By: [Carlos M Bustillo Rdguez](https://linkedin.com/in/carlosbustillordguez/)
---
+++ ecs-one-off-task.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+#
+# Creates a one-off ECS task.
+#
+# Usage:
+#   ./ecs-one-off-task.sh "TASK_NAME" "EXEC_ROLE_ARN" "ECS_CLUSTER_NAME" "OCI_IMAGE:TAG" "S3_ARN_ENV_VARS" "ENTRYPOINT" "COMMAND"
+#
+# Example:
+#   ecs-one-off-task.sh "db-migrations" "arn:aws:iam::ACCOUNT_ID:role/ecsTaskExecutionRole" "myEcsCluster" "myapp:latest" "arn:aws:s3:::bucket/myapp-vars.env" "sh -c" "bundle exec rake db:migrate"
+# 
+# By Carlos Miguel Bustillo Rdguez <https://linkedin.com/in/carlosbustillordguez/>
+# Speedyrails Inc. <https://www.speedyrails.com/>
+#
+
+
+### Functions
+
+check_requirements() {
+
+    if [ -z "$(which aws)" ]; then
+        echo "The command 'aws' is not installed in the system!!"
+        echo "Please follow the instructions: https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html"
+        exit 1
+    fi
+
+    if [ -z "$(which jq)" ]; then
+        echo "The command 'jq' is not installed in the system!!"
+        echo "Please install it: apt install jq"
+        exit 1
+    fi
+
+} # => check_requirements()
+
+create_task_definition_file() {
+
+    TASK_NAME="$1"
+    EXEC_ROLE_ARN="$2"
+    IMAGE_WITH_TAG="$3"
+    S3_ARN_ENV_VARS="$4"
+    ENTRYPOINT="$5"
+    COMMAND="$6"
+
+    # Convert ENTRYPOINT in a comma separated list
+    LIST=""
+    for i in $ENTRYPOINT; do
+        LIST="${LIST:+$LIST }\"$i\""
+    done
+    # Bash pattern substitution: ${parameter//pattern/string}
+    ENTRYPOINT_LIST=${LIST// /, }
+
+    # Convert COMMAND in a comma separated list
+    LIST=""
+    for i in $COMMAND; do
+        LIST="${LIST:+$LIST }\"$i\""
+    done
+    # Bash pattern substitution: ${parameter//pattern/string}
+    COMMAND_LIST=${LIST// /, }
+
+cat <<EOF > one-off-task-definition.json
+{
+    "executionRoleArn": "$EXEC_ROLE_ARN",
+    "containerDefinitions": [
+      {
+        "environmentFiles": [
+          {
+            "value": "$S3_ARN_ENV_VARS",
+            "type": "s3"
+          }
+        ],
+        "entryPoint": [ $ENTRYPOINT_LIST ],
+        "portMappings": [],
+        "command": [ $COMMAND_LIST ],
+        "cpu": 128,
+        "memory": 400,
+        "memoryReservation": 300,
+        "volumesFrom": [],
+        "image": "$IMAGE_WITH_TAG",
+        "name": "$TASK_NAME"
+      }
+    ],
+    "family": "$TASK_NAME"
+}
+EOF
+
+} # => create_task_definition_file()
+
+### Main Program
+
+## Check arguments
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ] || [ -z "$7" ]; then
+    echo "$(basename "$0"): Missing arguments or not arguments passed."
+    echo "Usage:"
+    echo "  ./$(basename "$0") \"TASK_NAME\" \"EXEC_ROLE_ARN\" \"ECS_CLUSTER_NAME\" \"OCI_IMAGE:TAG\" \"S3_ARN_ENV_VARS\" \"ENTRYPOINT\" \"COMMAND\""
+    echo "Example:"
+    echo "  ./$(basename "$0") \"db-migrations\" \"arn:aws:iam::ACCOUNT_ID:role/ecsTaskExecutionRole\" \"myEcsCluster\" \"myapp:latest\" \"arn:aws:s3:::bucket/myapp-vars.env\" \"sh -c\" \"bundle exec rake db:migrate\""
+    exit 1
+else
+    TASK_NAME="$1"
+    EXEC_ROLE_ARN="$2"
+    ECS_CLUSTER_NAME="$3"
+    IMAGE_WITH_TAG="$4"
+    S3_ARN_ENV_VARS="$5"
+    ENTRYPOINT="$6"
+    COMMAND="$7"
+fi
+
+check_requirements
+create_task_definition_file "$TASK_NAME" "$EXEC_ROLE_ARN" "$IMAGE_WITH_TAG" "$S3_ARN_ENV_VARS" "$ENTRYPOINT" "$COMMAND"
+
+# Register the task definition
+TASK_OUTPUT=$(aws ecs register-task-definition --cli-input-json file://one-off-task-definition.json)
+
+# Get the task definition ARN
+TASK_DEFINITION_ARN=$(echo "$TASK_OUTPUT" | jq -r '.taskDefinition.taskDefinitionArn')
+
+if [ -n "$TASK_OUTPUT" ]; then
+    echo "Created task definition: $TASK_DEFINITION_ARN"
+else
+    exit 1
+fi
+
+# Get the task definition revision
+TASK_DEFINITION_REVISION=$(echo "$TASK_OUTPUT" | jq -r '.taskDefinition.revision')
+
+# Run the task definition revision
+RUN_TASK_OUTPUT=$(aws ecs run-task --cluster "$ECS_CLUSTER_NAME" --task-definition "$TASK_NAME":"$TASK_DEFINITION_REVISION")
+
+# Get the task ARN
+RUN_TASK_ARN=$(echo "$RUN_TASK_OUTPUT" | jq -r '.tasks[0].taskArn')
+
+echo "Executed task ARN: $RUN_TASK_ARN"
+
+# Wait until the task is stopped
+aws ecs wait tasks-stopped --cluster "$ECS_CLUSTER_NAME" --tasks "$RUN_TASK_ARN"
+
+# Get the output of the stopped task
+STOPPED_TASK_OUTPUT=$(aws ecs describe-tasks --cluster "$ECS_CLUSTER_NAME" --tasks "$RUN_TASK_ARN")
+
+echo -e "The migration process has finised: \n"
+echo "$STOPPED_TASK_OUTPUT"
+
+# Get the container exit status code
+CT_EXIT_STATUS_CODE="$(echo "$STOPPED_TASK_OUTPUT" | jq -r '.tasks[0].containers[0].exitCode')"
+
+if [ "$CT_EXIT_STATUS_CODE" == "0" ]; then
+    echo -e "\nThe one-off task process has finished correctly!!"
+    exit 0
+else
+    echo -e "\nThe one-off task has failed. See the above output for further details!!"
+    exit 1
+fi
